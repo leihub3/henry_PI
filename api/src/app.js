5c74c846f153cc9f4300ab44e461345f035f4d4d
leihub3 <isaia.luis@gmail.com>
2021-12-23 20:35:10 -0300

    new css changes

--- api/src/app.js
+++ api/src/app.js
@@ -3,17 +3,19 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-//const cors = require('cors')
 //const {videogames} = require('./routes/videogames')
 //const addgame = require('./routes/addgame')
 
 require('./db.js');
 
 const server = express();
+const cors = require('cors')
 
 //server.use(express.json());
 
 server.name = 'API';
+
+server.use(cors())
 
 //server.use(express.json());
 
@@ -26,7 +28,7 @@ server.use(morgan('dev'));
 
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); //http://localhost:3000 update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); //http://localhost:3000 update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');//
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
