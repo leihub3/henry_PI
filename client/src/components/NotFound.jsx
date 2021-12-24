81c778128bdeed2a578979da7d040187857d0b8d
leihub3 <isaia.luis@gmail.com>
2021-12-24 08:31:14 -0300

    more host changes

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -1,12 +1,13 @@
 import './NotFound.css'
 export default function NotFound(){
+    /**http://localhost:3000 */
     return(
         <div id='notfoundContainer'>
             <div id='divImg'></div>
             <div id='message'>
                 <h1>404</h1>
             <p>Sorry...page not found</p>
-            <button onClick={() => window.location='http://localhost:3000/videogames/home'}>Go Home... </button>
+            <button onClick={() => window.location='/videogames/home'}>Go Home... </button>
             </div>
             
         </div>
