d22eba30156cdd1f14396760eebb21658527e66b
leihub3 <isaia.luis@gmail.com>
2021-12-26 09:17:13 -0300

    redux dev tools changes

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -76,6 +76,7 @@ function reducer(state = initialState, action){
 
   //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), 
 
-const store = createStore(reducer,compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(reducer,compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f)); 
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   
 export default store; 
